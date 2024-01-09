d2f40c418099c70af2595db3c9f21e1f698f685a
hoangsang03 <nguyenhoangsangtth2@gmail.com>
2024-01-10 01:07:16 +0700

    update login & register razor page

---
+++ WonderTour.Client/Model/TourDto.cs
@@ -0,0 +1,19 @@
+﻿namespace WonderTour.Client.Model
+{
+    public class TourDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool IsAdded { get; set; }
+    }
+}
